c708366fb81011ee97c038e78e5d4561ceacac73
Ilham_Ramadan <rilham702@gmail.com>
2026-06-28 02:48:49 +0700

    fix supabase legacy key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://klaveowqwvtxrwrrhqmu.supabase.co'
-const supabaseKey = 'sb_publishable_4YORwQqq13PXg1qzSl4Irw_aEXYUSDC'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYXZlb3dxd3Z0eHJ3cnJocW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NzY4MDcsImV4cCI6MjA5ODE1MjgwN30.lkPmCxyKLtbdxrmfuwiRkOkP6lkRhfteYmXq7Nkn1GY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
